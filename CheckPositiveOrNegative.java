19ce027fd379f5ad5dae89170ff6e0c39b91021c
Akshatabhagate <108929118+Akshatabhagate@users.noreply.github.com>
2022-07-21 16:58:11 +0530

    Add files via upload

---
+++ CheckPositiveOrNegative.java
@@ -0,0 +1,19 @@
+class CheckPositiveOrNegative
+{
+	public static void main(String[] ar)
+
+        {
+          
+             int num=100;
+             if(num>0)
+            {
+              System.out.println("The number is Positive");
+             }
+            else if(num<0)
+            {
+              System.out.println("The number is Negative");
+             }
+            
+        }
+
+}
